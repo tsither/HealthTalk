cc9947261e48bc636ac9c5a59ba15b09fed4f9a8
karasjova <89994591+karasjova@users.noreply.github.com>
2024-07-11 01:34:49 +0300

    Update urls.py

--- desktop_app/ui/urls.py
+++ desktop_app/ui/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('page1/', views.page1_view, name='page1'),
     path('page2/', views.page2_view, name='page2'),
     path('page3/', views.page3_view, name='page3'),
-    # path('chatbot/', views.chatbot_view, name='chatbot'),
+    path('page3_3/', views.page3_3, name='page3_3'),
 ]
