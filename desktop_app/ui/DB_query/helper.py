ae1d2e4eca994e1a1c7956b29433720585425357
tsither <sitherted1@gmail.com>
2024-08-16 11:07:45 +0200

    added RAG method to chatbot, organized database

--- desktop_app/ui/DB_query/helper.py
+++ desktop_app/ui/DB_query/helper.py
@@ -8,6 +8,8 @@ Date: 2024-06-09
 Notes: Current implementation only works with models from Anyscale AI api
 """
 
+import json
+
 ################################################################################################
 ################################################################################################
 
@@ -49,11 +51,26 @@ SQL Execution: {executed_query}
 
 Response:
 """
+
 ################################################################################################
 ################################################################################################
 
+RAG_CONTEXT = """
+You are a helpful medical assistant. 
 
+Given the user question and the user medical data, answer the user's question to the best of your ability. The data is an sql database in JSON format. The data is the users personal medical data, so the user may use personal pronouns like 'my' when querying. It is okay to say you do not have the information the user is looking for.
 
+Answer the users question to the best of your ability, ensuring to ONLY respond with information you find in the provided dataset. 
+
+User question: {question}
+User medical data: {user_data}
+
+"""
+
+################################################################################################
+################################################################################################
+
+ 
 def get_schema(db):
     """
     Gets schema from SQL database (db)
@@ -112,3 +129,39 @@ def generate_response(llm, query, template, question, db):
     response = llm.chat_completion(formatted_template, question)    #generate llm response
 
     return response
+
+
+
+def generate_RAG_query(llm, template, user_data, question):
+    """
+    Generates an SQL query.
+
+    Parameters:
+    llm (Class): Class instance of large language model
+    template (str): Prompt for llm to follow
+    question (str): User's question for the database
+
+    Returns:
+    out (str): LLM response
+    """
+
+    prompt = template.format(question=question, user_data=user_data)       #format template to include all necessary information (schema, question)
+    answer = llm.chat_completion(prompt, question)         #generates sql query
+
+    return answer
+
+
+def read_json_file(file_path):
+    """
+    Reads a JSON file and returns its contents as a dictionary.
+
+    Parameters:
+    file_path (str): The path to the JSON file.
+
+    Returns:
+    dict: The contents of the JSON file as a dictionary.
+    """
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
