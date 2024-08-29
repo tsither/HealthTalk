f8f8948a171d014fc1a39317d792758b6bcca1d0
Azimuth User <leonnial@hu-berlin.de>
2024-08-29 01:53:35 +0000

    running on VM

--- backend/content_extractor/testing.sh
+++ backend/content_extractor/testing.sh
@@ -11,7 +11,6 @@ source "/data/Personal-Medical-Assistant/backend/content_extractor/ocr_venv/bin/
 python3 -u /data/Personal-Medical-Assistant/backend/content_extractor/ocr.py > ./logs/ocr.out
 deactivate
 
-
 echo "Postprocessing!"
 
 source "/data/Personal-Medical-Assistant/backend/content_extractor/postprocessing_venv/bin/activate"
@@ -24,6 +23,5 @@ source "/data/Personal-Medical-Assistant/backend/content_extractor/evaluate_venv
 python3 -u /data/Personal-Medical-Assistant/backend/content_extractor/evaluate.py > ./logs/evaluation.out
 deactivate
 
-
-echo "Testing done!"
+echo "Done done!"
 
